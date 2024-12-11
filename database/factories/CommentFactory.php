6fbf78a7226fc9992cdd60575606607517785f7f
studentlucas <338362@student.mboutrecht.nl>
2024-12-11 11:25:05 +0100

    2de commit

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
+
+use Faker\Generator as Faker;
+
+ 
+
+$factory->define(Comment::class, function (Faker $faker) {
+
+    return [
+
+        'blog_id' => Blog::factory(),
+
+        'content' => $faker->sentence,
+
+    ];
+
+});
